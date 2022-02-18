866aa6e5dda9fab18234c11d32aaed34813e9019
Alban30so <alban.trentesaux@gmail.com>
2022-02-18 11:32:47 +0100

    Dernier essai

--- ProjetI1-2/main.cpp
+++ ProjetI1-2/main.cpp
@@ -1,11 +1,30 @@
-#include <SDL.h>
+#include <SDL/SDL.h>
 #include <stdio.h>
-#include <stdlib.h>
-
-int main (int argc, char **argv){
-    SDL_Init(SDL_INIT_EVERYTHING);
-
-
+#define OUI 1
+#define NON 0
+void pause(){
+    int continuer=OUI;
+    SDL_Event event;
+    while(continuer==OUI){
+        SDL_WaitEvent(&event);
+        if(event.type==SDL_QUIT){
+            continuer=NON;
+        }
+    }
+}
+int main (int argc, char**argv){
+    SDL_Surface*ecran,*newrectangle,*fond;
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Rect position;
+    ecran=SDL_SetVideoMode(800,600,32,SDL_HWSURFACE);
+    SDL_WM_SetCaption("Ecran de jeu",NULL);
+    Uint32 blanc=SDL_MapRGB(ecran->format,255,255,255);
+    SDL_FillRect(ecran,NULL,blanc);
+    newrectangle=SDL_CreateRGBSurface(SDL_HWSURFACE,100,100,32,0,0,0,0);
+    position.x=400-150;position.y=300-100;
+    SDL_BlitSurface(newrectangle,NULL,ecran,&position);
+    SDL_Flip(ecran);
+    pause();
     return 0;
 }
 /*
@@ -21,9 +40,8 @@ int main ( int argc, char** argv ){
         printf("Unable to load bitmap: %s\n", SDL_GetError());
         return 1;
     }
-
     // centre the image
-    SDL_Rect dstrect={0,0,640,480};
+    SDL_Rect dstrect={0,0,100,100};
 
     // program main loop
     bool done = false;
@@ -69,3 +87,24 @@ int main ( int argc, char** argv ){
     SDL_FreeSurface(bmp);
     return 0;
 }
+
+#include <stdio.h>
+#include <SDL2/SDL.H>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+int main(int argc, char** argv){
+    if(SDL_Init(SDL_INIT_EVERYTHING)<0){
+        printf("Error : SDL failed to initialize");
+        return 1;
+    }
+    SDL_Window *window=SDL_CreateWindow("Jeu de dames chinois", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT,0);
+    if(!window){
+        printf("Error: Failed to open window\n SDL Error:'%s'\n",SDL_GetError());
+        return 1;
+    }
+
+    return 0;
+}
+*/
